49d3ef8fa42409f2ad0baaac7740958dc16661e8
rafethander <handerrafet@gmail.com>
2021-03-13 16:54:56 +0300

    Stok Eklendi

---
+++ Models/Stok.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekProjeGitGithub.Models
+{
+    public class Stok
+    {
+        public int Id { get; set; }
+        public string StokAdi { get; set; }
+        public string StokKodu { get; set; }
+    }
+}

4f5761abcd62d681e96ab8a4ad07bb0d2233bc04
rafethander <handerrafet@gmail.com>
2021-03-13 18:35:08 +0300

    kullanici eklendi

---
+++ Models/KullaniciTanim.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekProjeGitGithub.Models
+{
+    public class KullaniciTanim
+    {
+        public int KullaniciId { get; set; }
+        public string AdiSoyadi { get; set; }
+    }
+}

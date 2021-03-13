5e37d7e13501c80226120da2834572cbb7106e60
rafethander <handerrafet@gmail.com>
2021-03-13 18:45:08 +0300

    fix

---
+++ Models/IsEmriTanim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekProjeGitGithub.Models
+{
+    public class IsEmriTanim
+    {
+        public int Id { get; set; }
+        public string IsEmriNo { get; set; }
+        
+    }
+}
